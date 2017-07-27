f8ad323f2034924924eaf1e88bb47c5887c6b7e3
DharanyaN <30457114+DharanyaN@users.noreply.github.com>
2017-07-27 14:09:22 +0530

    Update big.c

--- big.c
+++ big.c
@@ -4,10 +4,12 @@ int main()
 int a,b,c;
 printf("Enter 3 values");
 scanf("%d %d %d",&a,&b,&c);
-if(a>b&&a>c)
-  printf("a is greater");
- elseif(b>a&&b>c)
-  printf("b is greater");
- elseif(c>a&&c>b)
-  printf("c is greater");
-  }
+if((a>b)&&(a>c))
+    printf("\n a is greater");
+ else if((b>a)&&(b>c))
+    printf("\n b is greater");
+ else if((c>a)&&(c>b))
+    printf("\n c is greater");
+    else
+    printf("invalid input");
+}
